a456b5e8be2647552c7422c1af60bf90bbccc221
AravindElangovan <filch20@gmail.com>
2015-04-25 17:24:13 -0400

    Created game server object.

---
+++ src/main/java/edu/brown/cs/pdtran/minesweep/metagame/GameServer.java
@@ -0,0 +1,15 @@
+package edu.brown.cs.pdtran.minesweep.metagame;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import com.google.gson.stream.JsonReader;
+
+public class GameServer extends WebSocketServer {
+
+
+}
