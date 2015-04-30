f928c6814451180f141122d02391d300ff609ad9
Clayton Sanford <clayton_sanford@brown.edu>
2015-04-30 13:02:48 -0400

    Checkstyle adventure (part 1)

---
+++ src/main/java/edu/brown/cs/pdtran/minesweep/board/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains all classes that represent boards for the game. Each type of board
+ * with different shapes of tiles (Default, Rectangular, Triangular, Entangled,
+ * Hexagonal) has a class representing its properties in this package.
+ */
+
+package edu.brown.cs.pdtran.minesweep.board;
