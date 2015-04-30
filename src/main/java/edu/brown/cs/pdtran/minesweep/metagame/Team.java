2917274fd01bdeae502b7a4f384f228adb5b05dc
Clayton Sanford <clayton_sanford@brown.edu>
2015-04-30 15:51:29 -0400

    Checkstyle adventure (part 6)

--- src/main/java/edu/brown/cs/pdtran/minesweep/metagame/Team.java
+++ src/main/java/edu/brown/cs/pdtran/minesweep/metagame/Team.java
@@ -4,20 +4,41 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * An abstract class representing a Team.
+ * @author Clayton Sanford
+ */
 public abstract class Team {
 
   protected String name;
 
+  /**
+   * Constructs a Team object.
+   * @param name The name that represents the given team.
+   */
   public Team(String name) {
     this.name = name;
   }
 
+  /**
+   * Gets the name of the team.
+   * @return A string representing the name of the Team.
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * An abstract method that produces a map between strings and players.
+   * @return A map that relates unique strings and human or AI players.
+   */
   public abstract Map<String, ? extends Player> getPlayers();
 
+  /**
+   * Gets the TeamInfo object corresponding to the Team, which is created
+   * using a map of all PlayerInfo objects.
+   * @return The TeamInfo object corresponding to the Team.
+   */
   public TeamInfo getTeamInfo() {
     List<PlayerInfo> playersInfo = new ArrayList<PlayerInfo>();
     for (Player player : getPlayers().values()) {
--- src/main/java/edu/brown/cs/pdtran/minesweep/metagame/TeamInfo.java
+++ src/main/java/edu/brown/cs/pdtran/minesweep/metagame/TeamInfo.java
@@ -2,19 +2,39 @@ package edu.brown.cs.pdtran.minesweep.metagame;
 
 import java.util.List;
 
+/**
+ * Contains the information necessary for a Team object, which is its name
+ * and the PlayerInfo for all of its players.
+ * @author Clayton Sanford
+ */
 public class TeamInfo {
   private String name;
   private List<PlayerInfo> players;
 
+  /**
+   * Constructs a TeamInfo object.
+   * @param name A string representing the team's name.
+   * @param players A list of PlayerInfo objects corresponding to all
+   *        players in the Team.
+   */
   public TeamInfo(String name, List<PlayerInfo> players) {
     this.name = name;
     this.players = players;
   }
 
+  /**
+   * Gets the name of the Team.
+   * @return A string representing the name of the Team.
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * Gets all the corresponding player info.
+   * @return A list of PlayerInfo objects corresponding to all players in
+   *         the Team.
+   */
   public List<PlayerInfo> getPlayers() {
     return players;
   }
