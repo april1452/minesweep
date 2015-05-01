cb763905f840dae381fca785ed8514a32ebc3f16
Aaron Gokaslan <Skylion.aaron@gmail.com>
2015-05-01 11:37:40 -0400

    Fixed triangular issue.

--- src/main/java/edu/brown/cs/pdtran/minesweep/board/HexagonalBoard.java
+++ src/main/java/edu/brown/cs/pdtran/minesweep/board/HexagonalBoard.java
@@ -5,16 +5,12 @@ import java.util.List;
 
 import edu.brown.cs.pdtran.minesweep.tile.Tile;
 
-/**
- * This class implements triangular boards for iterations to the game.
- * @author agokasla
- */
-public class TriangularBoard extends DefaultBoard implements Board, Cloneable {
+public class HexagonalBoard extends DefaultBoard implements Board, Cloneable {
 
   /**
    * The constructor.
    */
-  public TriangularBoard() {
+  public HexagonalBoard() {
     super();
   }
 
@@ -22,7 +18,7 @@ public class TriangularBoard extends DefaultBoard implements Board, Cloneable {
    * The Constructor.
    * @param grid the tile grid. Use only for testing.
    */
-  public TriangularBoard(Tile[][] grid) {
+  public HexagonalBoard(Tile[][] grid) {
     super(grid);
   }
 
@@ -30,13 +26,18 @@ public class TriangularBoard extends DefaultBoard implements Board, Cloneable {
   public List<Tile> getAdjacentTiles(int row, int col) {
     List<Tile> tiles = new ArrayList<>(3);
 
-    boolean goDown = col % 2 == 1; // decides which side the isoceles
-                                   // triangle
+    boolean isIndented = col % 2 == 1; // decides which side the isoceles
+    // triangle
     // is.
-    int newRow = goDown ? row - 1 : row + 1;
+    int newCol = isIndented ? col - 1 : col + 1;
 
-    if (isWithinBoard(newRow, col)) {
-      tiles.add(grid[newRow][col]);
+    for (int i = -1; i <= 1; i += 2) {
+      if (isWithinBoard(row + i, col)) {
+        tiles.add(grid[row + i][col]);
+      }
+      if (isWithinBoard(row + i, newCol)) {
+        tiles.add(grid[row + i][newCol]);
+      }
     }
 
     for (int i = -1; i <= 1; i++) {
@@ -48,14 +49,14 @@ public class TriangularBoard extends DefaultBoard implements Board, Cloneable {
   }
 
   @Override
-  public TriangularBoard clone() {
+  public HexagonalBoard clone() {
     Tile[][] newGrid = new Tile[getHeight()][getWidth()];
     for (int i = 0; i < newGrid.length; i++) {
       for (int j = 0; j < grid[0].length; j++) {
         newGrid[i][j] = grid[i][j].clone();
       }
     }
-    return new TriangularBoard(newGrid);
+    return new HexagonalBoard(newGrid);
   }
 
 }
