4d4d316d94e31c6b4ea83d4b234e2e66e06d8f08
Phuc Anh (April) Tran <april_tran@brown.edu>
2015-05-05 03:20:14 -0400

    leave/disband working

--- src/main/resources/static/js/play.js
+++ src/main/resources/static/js/play.js
@@ -224,9 +224,9 @@ function drawRoom(responseJson) {
         // have to readd the sidebar; css issues
         var sidebar;
         if (isHost) {
-            sidebar = '<div id="start" class="span-2"><a class="button line-white large" id="startButton">Start Game!</a><a href="/" class="button line-white large" id="disbandButton">Disband</a></div>';
+            sidebar = '<div id="start" class="span-2"><a class="button line-white large" id="startButton">Start Game!</a><a class="button line-white large" id="disbandButton">Disband</a></div>';
         } else {
-            sidebar = '<div id="start" class="span-2"><a href="/" class="button line-white large" id="leaveButton">Leave Room</a></div>';
+            sidebar = '<div id="start" class="span-2"><a class="button line-white large" id="leaveButton">Leave Room</a></div>';
         }
 
         $("#teams").html(sidebar + innerBox);
@@ -253,6 +253,7 @@ function drawRoom(responseJson) {
                     minesweepRoomId: $.cookie("minesweepRoomId")
                 };  
                 socket.send(JSON.stringify(sendData));
+                window.location.href = '/';
             });
         });
 
@@ -266,6 +267,7 @@ function drawRoom(responseJson) {
                     minesweepRoomId: $.cookie("minesweepRoomId")
                 };  
                 socket.send(JSON.stringify(sendData));
+                window.location.href = '/';
             });
         });
         
